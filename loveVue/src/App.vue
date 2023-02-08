eb8407522f98de8463fe00afe1168c338486df2b
AstralBoy <baratashvili.beka@kiu.edu.ge>
2023-02-08 14:50:13 +0400

    next, prev, enis shecvla da fotoebis raodenobis cvlileba

--- loveVue/src/App.vue
+++ loveVue/src/App.vue
@@ -1,74 +1,91 @@
 <script>
-import ToDoList from './ToDoList.vue'
-import ToDoitemForm from './ToDoitemForm.vue'
+
+import axios from 'axios'
+
 export default {
-    components: {
-        ToDoList ,
-        ToDoitemForm
-    },
+    
     data() { 
       return {
-        search: null,
-        todos: [
-          { id: 1, name: "to do item", done: false },
-          { id: 2, name: "to do item 2", done: false },
-          { id: 3, name: "to do item 3", done: false },
-        ]
+        langs: ['Geo', 'Eng'],
+        range: [4,5,10,20],
+        limit: 10,
+        lang: 'ge',
+        apiUrl: "http://items.magischer.de/api/products" ,
+        products: [],
+        res: null,
       }
     },
-   methods: {
-    putNewItemIntoTodoList(data){
-      this.todos.push({
-        id: this.nextId,
-        name: data,
-        done: false
-      })
+    methods: {
+
+      getDataFromApiUrl(url = this.apiUrl) {
+        axios.get(url, {
+          params: { 
+            limit: this.limit, 
+            lang: this.lang 
+          }
+          }).then((response) => { 
+          this.res = response.data 
+          this.products = response.data.data
+        })
+      },
+      nextPage() {
+        this.getDataFromApiUrl(this.res?.next_page_url)
+      },
+      prevPage() {
+        this.getDataFromApiUrl(this.res?.prev_page_url)
+      },
+      firstPage() {
+        this.getDataFromApiUrl(this.res?.first_page_url)
+      },
+      lastPage() {
+        this.getDataFromApiUrl(this.res?.last_page_url)
+      },
+      limitChange(e) {
+        this.limit = e.target.value
+        this.getDataFromApiUrl()
+      },
+      changeLang() {
+        this.lang = e.target.lang
+        this.getDataFromApiUrl()
+      },
     },
-    changeStatus(record){
-      let v = this.todos.find(item=> item.id === record.id)
-       v.done = !v.done
-    },
-    deleteItemFromTodoList(record){
-      let v = this.todos.find(item => item.id === record.id)
-      this.todos.splice(this.todos.indexOf(v), 1)
-    }
-   },
-   computed: {
-    nextId(){
-      return this.todos.length + 1
-    },
-    activeTodos(){
-      return this.todos.filter(value=> value.done === false)
-    },
-    doneTodos(){
-      return this.todos.filter(value=> value.done === true)
-    },
-    filteredTodos(){
-      return (this.search != null && this.search.length > 2) 
-        ? this.todos.filter(value=> value.name.toLocaleLowerCase().includes(this.search.toLocaleLowerCase()))
-        : this.todos
-    }
+    mounted() {
+    this.getDataFromApiUrl()
    }
 }
 </script>
 
 <template>
-  <div class="container">
-    <div class="todos">
-      <ToDoitemForm @onSubmit="putNewItemIntoTodoList($event)"/>
-      <h2>All Todo Items</h2>
-      <ToDoList :data="todos"/>
-
-      <h2>Active Todos</h2>
-      <ToDoList :changeStatus="true" :data="activeTodos" @onChangeStatus="changeStatus($event)"/>
-
-      <h2>ToDo With Done Status</h2>
-      <ToDoList :canDelete="true" :data="doneTodos" @onDelete="deleteItemFromTodoList($event)" />
-    </div>
-    <div class="filter">
-      <h2>Filter</h2>
-      <input type="text" v-model="search"/>
-      <ToDoList :data="filteredTodos"/>
-    </div>
+  <!-- <h2 class="text-3xl font-bold underline">here {{ apiUrl }}</h2> -->
+  <select @change="limitChange">
+    <option v-for="amount in range" :key="amount.index" :value="amount">Per Page: {{ amount }}</option>
+  </select>
+  <br>
+  <select @change="changeLang">
+    <option v-for="amount in langs" :key="amount.index" :value="amount">Change Language: {{ amount }}</option>
+  </select>
+  <br>
+  <table class="table-auto">
+    <thead>
+      <th>Product id</th>
+      <th>Product name</th>
+      <th>cover img</th>
+    </thead>
+    <tbody>
+      <tr v-for="item in products" :key="item.index">
+        <td>{{ item.id }}</td>
+        <td>{{ item.name }}</td>
+        <td>
+          <img style="width: 200px; height: auto;" :src="item.img.cover" /> 
+        </td>
+      </tr>
+    </tbody>
+  </table>
+  <br>
+  <div style="display: flex; justify-content: between;">
+    <a href="#" :disabled="!res?.first_page_url" @click.prevent="firstPage" >FirstPage</a>
+    <button :disabled="!res?.prev_page_url" @click.prevent="prevPage">Prev</button>
+    <button :disabled="!res?.next_page_url" @click.prevent="nextPage">Next</button>
+    <a href="#" :disabled="!res?.last_page_url" @click.prevent="lastPage" >LastPage</a>
   </div>
 </template>
